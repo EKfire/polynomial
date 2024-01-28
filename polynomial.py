dd45c8c5151925fc62f08784046d11434f8a3c47
Qi Guo <guoqiwin@126.com>
2024-01-28 16:51:12 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,20 +5,31 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, value):
+        return value
+
+
 class Int:
     def __init__(self, i):
         self.i = i
-    
+
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, value):
+        return self.i
+
 
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 
 class Mul:
@@ -27,31 +38,17 @@ class Mul:
         self.p2 = p2
 
     def __repr__(self):
-        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, Add) or isinstance(self.p1, Sub) else repr(self.p1)
-        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
-        return f"{p1_repr} * {p2_repr}"
+        if isinstance(self.p1, Add):
+            if isinstance(self.p2, Add):
+                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Add):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " * " + repr(self.p2)
 
-class Div:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def __repr__(self):
-        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, Add) or isinstance(self.p1, Sub) else repr(self.p1)
-        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
-        return f"{p1_repr} / {p2_repr}"
-
-class Sub:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def __repr__(self):
-        p1_repr = repr(self.p1)
-        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
-        return f"{p1_repr} - {p2_repr}"
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
 
 
-
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly.evaluate(1))
