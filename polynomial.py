cd834920932b1f75e0e2fdaaf844b4738a9c9c5c
Qi Guo <guoqiwin@126.com>
2024-01-28 16:40:05 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -20,19 +20,37 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, Add) or isinstance(self.p1, Sub) else repr(self.p1)
+        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
+        return f"{p1_repr} * {p2_repr}"
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        p1_repr = f"( {repr(self.p1)} )" if isinstance(self.p1, Add) or isinstance(self.p1, Sub) else repr(self.p1)
+        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
+        return f"{p1_repr} / {p2_repr}"
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        p1_repr = repr(self.p1)
+        p2_repr = f"( {repr(self.p2)} )" if isinstance(self.p2, Add) or isinstance(self.p2, Sub) else repr(self.p2)
+        return f"{p1_repr} - {p2_repr}"
+
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
